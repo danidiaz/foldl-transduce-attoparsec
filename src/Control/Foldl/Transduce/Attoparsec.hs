9f7ca305cc5ecd7e2f8b3b0132d59e4290b307f4
ddc <diaz_carrete@yahoo.com>
2015-09-16 23:16:24 +0200

    ParserInput

--- src/Control/Foldl/Transduce/Attoparsec.hs
+++ src/Control/Foldl/Transduce/Attoparsec.hs
@@ -1,3 +1,61 @@
 module Control.Foldl.Transduce.Attoparsec (
-
+        ParserInput
     ) where
+
+import qualified Data.ByteString as B
+import qualified Data.Text as T
+import qualified Data.Attoparsec.ByteString
+import qualified Data.Attoparsec.Text
+
+import qualified Data.Monoid.Factorial as MF
+
+import Data.Attoparsec.Types
+import Data.Attoparsec.ByteString
+import Data.Attoparsec.Text
+
+class (Eq a,MF.StableFactorialMonoid a) => ParserInput a where
+    _parse :: Data.Attoparsec.Types.Parser a b -> a -> IResult a b
+
+instance ParserInput B.ByteString where
+    _parse  = Data.Attoparsec.ByteString.parse
+    {-# INLINE _parse #-}
+
+-- | Strict 'Text'.
+instance ParserInput T.Text where
+    _parse  = Data.Attoparsec.Text.parse
+    {-# INLINE _parse #-}
+
+--
+----------------------------------------------------------------------------------
+--
+---- | A parsing error report, as provided by Attoparsec's 'Fail'.
+--data ParsingError = ParsingError
+--    { peContexts :: [String]  -- ^ Contexts where the parsing error occurred.
+--    , peMessage  ::  String   -- ^ Parsing error description message.
+--    } deriving (Show, Read, Eq, Data, Typeable)
+--
+--instance Exception ParsingError
+--instance Error     ParsingError
+--
+---- | This instance allows using 'Pipes.Lift.errorP' with 'parsed' and 'parsedL'
+--instance Error (ParsingError, Producer a m r)
+--
+----------------------------------------------------------------------------------
+---- Internal stuff
+--
+---- | Like 'Pipes.next', except it skips leading 'mempty' chunks.
+--nextSkipEmpty
+--  :: (Monad m, Eq a, Monoid a)
+--  => Producer a m r
+--  -> m (Either r (a, Producer a m r))
+--nextSkipEmpty = go where
+--    go p0 = do
+--      x <- next p0
+--      case x of
+--         Left  _        -> return x
+--         Right (a,p1)
+--          | a == mempty -> go p1
+--          | otherwise   -> return x
+--{-# INLINABLE nextSkipEmpty #-}
+--
+--
